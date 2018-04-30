2fc5e13481a5ba9658c035cd75794540b433d4dd
sabsteef <38842670+sabsteef@users.noreply.github.com>
2018-04-30 18:33:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -155,9 +155,9 @@ Thermostat.prototype = {
 			auth : this.auth
 		}, 	    
 			function(err, response, body) {
-			this.log(err);
-			this.log(response);
-			this.log(body);
+			this.log("error ", err);
+			this.log("Response ",response);
+			this.log("Body", body);
 			if (!err && response.statusCode == 200) {
 				this.log("response success");
 				var json = JSON.parse(body); //{targetHeatingCoolingState":3,"currentHeatingCoolingState":0,"temperature":"18.10","humidity":"34.10"}
